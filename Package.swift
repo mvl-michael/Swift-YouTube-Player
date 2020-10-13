19f88a9cd5492145b3a7f65f42c88a53483276a5
ha1f <half.ldk.hiz@gmail.com>
2020-10-13 15:58:25 +0900

    test package

---
+++ Package.swift
@@ -0,0 +1,32 @@
+// swift-tools-version:5.2
+import PackageDescription
+ 
+let package = Package(
+    name: "Swift-YouTube-Player",
+    platforms: [
+        .iOS(.v8),
+    ],
+    products: [
+        .library(
+            name: "YouTubePlayer",
+            targets: ["YouTubePlayer"]),
+    ],
+    dependencies: [
+        // no dependencies
+    ],
+    targets: [
+        .target(
+            name: "YouTubePlayer",
+            dependencies: [],
+            path: "YouTubePlayer/YouTubePlayer"
+            // resources: [
+            //   "YTPlayer.html"
+            // ]
+        ),
+        .target(
+            name: "YouTubePlayerExample",
+            dependencies: ["YouTubePlayer"],
+            path: "YouTubePlayerExample/YouTubePlayerExample"
+        )
+    ]
+)
