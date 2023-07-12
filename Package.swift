7d723674717d1415cf9dadc07b83a0e880b5ec7a
Michael.kang <michael.kang@mvlchain.io>
2023-07-12 12:19:26 +0900

    change minimum target

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 let package = Package(
     name: "Swift-YouTube-Player",
     platforms: [
-        .iOS(.v8),
+        .iOS(.v11),
     ],
     products: [
         .library(
